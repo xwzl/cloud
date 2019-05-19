7772b1335f0ca16baf75986a4c34db363dc136c9
徐伟智 <624244232@qq.com>
2019-05-20 01:46:55 +0800

    SpringCloud init

---
+++ spring-server/src/main/java/com/cloud/server/ServerApplication.java
@@ -0,0 +1,21 @@
+package com.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务端注册中心，@EnableEurekaServer
+ *
+ * @author xuweizhi
+ * @date 2019/05/20 0:25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
