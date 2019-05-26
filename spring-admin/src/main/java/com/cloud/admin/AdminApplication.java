dc0e53c3755aded1ac9943360644c5f1ed3eb86c
xuweizhi <624244232@qq.com>
2019-05-26 18:16:30 +0800

    SpringBoot Admin

---
+++ spring-admin/src/main/java/com/cloud/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuweizhi
+ * @since 2019/05/26 17:48
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
