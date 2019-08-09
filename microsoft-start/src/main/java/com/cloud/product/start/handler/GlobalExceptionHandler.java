e2f95c6262fcad3116f82fd803f6b0230d5c8438
xuweizhi <624244232@qq.com>
2019-08-09 19:55:45 +0800

    start spring

--- microsoft-start/src/main/java/com/cloud/product/start/handler/GlobalExceptionHandler.java
+++ microsoft-start/src/main/java/com/cloud/product/start/handler/GlobalExceptionHandler.java
@@ -1,6 +1,15 @@
 package com.cloud.product.start.handler;
 
+import com.cloud.product.start.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author xuweizhi
@@ -9,33 +18,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(Throwable.class)
-//    public ApiResult handleThrowable(Throwable e) {
-////        log.error("meet exception: " + e.getClass().getName() + " " + e.getMessage());
-//        log.error("meet exception: ", e);
-//        return new ApiResult(HttpStatusEnum.INTERNAL_SERVER_ERROR.value(),
-//                HttpStatusEnum.INTERNAL_SERVER_ERROR.getReasonPhrase());
-//    }
-//
-//    @ExceptionHandler(ApiException.class)
-//    public void handleApiException(ApiException e) throws IOException {
-//        log.error("meet ApiException: " + e.getCode() + " " + e.getMessage());
-//        ServletRequestAttributes servletRequestAttributes =
-//                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//        HttpServletResponse response = servletRequestAttributes.getResponse();
-//        response.sendError(e.getCode(), e.getMessage());
-//    }
-//
-//    @ExceptionHandler(ServiceException.class)
-//    public ApiResult<?> handleServiceException(ServiceException e) {
-//        log.error("meet ServiceException: " + e.getCode() + " " + e.getMessage());
-//        return new ApiResult(e);
-//    }
-//
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ApiResult<?> illegalArgumentException(IllegalArgumentException e) {
-//        log.error("meet ServiceException: " + e.getMessage());
-//        return new ApiResult<>(-1, e.getMessage());
-//    }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Throwable.class)
+    public ApiResult handleThrowable(Throwable e) {
+//        log.error("meet exception: " + e.getClass().getName() + " " + e.getMessage());
+        log.error("meet exception: ", e);
+        return new ApiResult(HttpStatusEnum.INTERNAL_SERVER_ERROR.value(),
+                HttpStatusEnum.INTERNAL_SERVER_ERROR.getReasonPhrase());
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public void handleApiException(ApiException e) throws IOException {
+        log.error("meet ApiException: " + e.getCode() + " " + e.getMessage());
+        ServletRequestAttributes servletRequestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = servletRequestAttributes.getResponse();
+        response.sendError(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ApiResult<?> handleServiceException(ServiceException e) {
+        log.error("meet ServiceException: " + e.getCode() + " " + e.getMessage());
+        return new ApiResult(e);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ApiResult<?> illegalArgumentException(IllegalArgumentException e) {
+        log.error("meet ServiceException: " + e.getMessage());
+        return new ApiResult<>(-1, e.getMessage());
+    }
 }
