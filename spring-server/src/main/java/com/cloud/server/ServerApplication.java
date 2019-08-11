7a93cee2a0a7bd75aff14c4136287741d1954ad9
xuweizhi <624244232@qq.com>
2019-08-11 23:11:07 +0800

    feign 全局处理异常

--- spring-server/src/main/java/com/cloud/server/ServerApplication.java
+++ spring-server/src/main/java/com/cloud/server/ServerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * 服务端注册中心，@EnableEurekaServer
  *
+ * Java 11 版本与 tomcat 相关的 jar包好像被删除了，额外引入项目才能启动项目 .......
+ *
  * @author xuweizhi
  * @date 2019/05/20 0:25
  */
